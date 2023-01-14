12ddfdc158551322db3b1ea0d1fb70c7cb2cecc0
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2023-01-14 19:21:30 -0400

    Project Variable created

---
+++ hello/main.py
@@ -0,0 +1,33 @@
+# Hola
+# This is a comment:
+# Our Hello world in Python
+print("Hola Python")
+print("Hello Python")
+print('Hello Python')
+
+# Print the data type which is String
+print(type("Hello Python"))  # Type 'str'
+
+# Check the data type
+print(type("Soy un dato str"))  # Type 'str'
+print(type(5))  # Type 'int'
+print(type(True))  # Type 'bool'
+print(type(1 +1j))  # Type 'complex'
+
+
+
+
+
+
+
+"""
+This is a 
+comment 
+in several lines
+"""
+
+'''
+This is a 
+comment 
+in several lines
+'''
