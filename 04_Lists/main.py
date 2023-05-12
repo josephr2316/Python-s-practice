788a2539f8bbd5318a26307436c400b68e37b11a
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2023-05-11 20:37:15 -0400

    Printing lists

--- 04_Lists/main.py
+++ 04_Lists/main.py
@@ -1,10 +1,20 @@
 ### Lists ###
 
-my_list = list()
+my_list = list()  # We don't have to initialize the variable
 my_other_list = []
 
 print(my_list)
+print(my_other_list)
 
+print(len(my_list))
+print(len(my_other_list))
 
+my_list = [35, 24, 62, 52, 30, 30, 17]
+print(my_list)
+print(len(my_list))
+
+my_other_list = [28, 1.85, "Joseph", "Rodríguez Kelly"]
+print(my_other_list)
+print(type(my_other_list))
 
 
