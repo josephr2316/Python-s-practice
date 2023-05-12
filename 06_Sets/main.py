adc015a5c27d9e169ea8f0acffdbed477d655fff
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2023-05-12 12:27:04 -0400

    Printing sets and declaring

--- 06_Sets/main.py
+++ 06_Sets/main.py
@@ -1,3 +1,19 @@
 ### Sets ###
 
+my_set = set()
+my_other_set = {}
+
+print(my_set)
+print(my_other_set)
+
+print(type(my_set))
+print(type(my_other_set))  # Initially is a dictionary
+
+my_other_set = {"Joseph", "Rodriguez Kelly", 28}
+print(type(my_other_set))
+
+print(len(my_other_set))
+print(my_other_set)
+# print(my_other_set[0]) # TypeError: 'set' object is not subscriptable
+
 
