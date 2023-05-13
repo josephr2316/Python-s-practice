5914670eec4dfccb6fc45b4a06a05cb7f4e39fcd
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2023-05-12 23:42:04 -0400

    Checking some functions

--- 06_Sets/main.py
+++ 06_Sets/main.py
@@ -16,4 +16,31 @@ print(len(my_other_set))
 print(my_other_set)
 # print(my_other_set[0]) # TypeError: 'set' object is not subscriptable
 
+my_other_set.add("Joseph Dev")
+print(my_other_set)  # A set is not an ordered structure, a hash to set all the elements
+
+my_other_set.add("Joseph Dev")  # A set does not admit repetitions
+print(my_other_set)
+
+print("Rodriguez Kelly" in my_other_set)
+print("Rodriguez" in my_other_set)
+
+'''
+my_other_set.remove("Rodriguez Kelly")  # Won't work, the element to be deleted has to be in the set
+print(my_other_set)
+'''
+
+my_other_set.clear()
+print(my_other_set)
+print(len(my_other_set))
+
+# del # Is from the system
+
+del my_other_set
+
+
+
+
+
+
 
