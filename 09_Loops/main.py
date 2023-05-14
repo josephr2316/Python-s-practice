67298f6d7920ab5067c012f9e0f75969278fd1c8
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2023-05-14 00:32:07 -0400

    Finished practising loops

--- 09_Loops/main.py
+++ 09_Loops/main.py
@@ -11,7 +11,6 @@ while my_condition < 10:  # We can use else, but it does not accept the others.
 else:  # It's optional
     print("My condition is greater or equal to 10")
 
-
 # if my_condition == 10:
 #     print("My condition is equal to 10")
 # else:
@@ -28,3 +27,53 @@ while my_condition < 20:
 print("The execution continues")
 
 # For
+
+my_list = [35, 24, 62, 52, 30, 30, 17]
+
+print("\n" + "List\n")
+for element in my_list:
+    print(element)
+
+my_tuple = (28, 1.85, "Joseph", "Rodriguez Kelly", "Joseph")
+
+print("\n" + "Tuple\n")
+for element in my_tuple:
+    print(element)
+
+my_set = {"Joseph", "Rodriguez Kelly", 28}
+
+print("\n" + "Set\n")
+for element in my_set:
+    print(element)
+
+my_dict = {"Joseph": "Joseph", "Surname": "Rodriguez Kelly", "Age": 28, 1:"Python"}
+
+print("\n" + "Dictionary Keys\n")
+for element in my_dict:
+    print(element)
+    if element == "Age":
+        break
+    print("It's executed")
+else:
+    print("The for loop for the dictionary has finished")
+
+print("\n" + "Dictionary Keys Part II\n")
+for element in my_dict:
+    print(element)
+    if element == "Age":
+        continue  # Continue to the for, stop the execution to this point.
+    """
+    It's like  
+     if element == "Age":
+     else:
+        print("It's executed")
+
+    """
+    print("It's executed")
+else:
+    print("The for loop for the dictionary has finished")
+
+print("\n" + "Dictionary Values\n")
+for element in my_dict.values():
+    print(element)
+
