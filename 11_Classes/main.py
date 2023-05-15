b3feb9390d1d855cfe638d7cca864a3dc73c8eb0
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2023-05-15 17:34:09 -0400

    Implementing pass in a class

--- 11_Classes/main.py
+++ 11_Classes/main.py
@@ -1,1 +1,19 @@
 ###  Classes ###
+
+class MyEmptyPerson:
+    # def __int__(self) -> None:  # def __int__(self):  # Empty constructor
+    #     pass
+    pass  # It will generate an error if there's no code inside, pass avoid this.
+
+
+class Person:
+    def __init__(self, name, surname):
+        pass
+
+
+print("Hello")
+print(MyEmptyPerson)
+print(MyEmptyPerson())
+
+my_person = Person("Joseph", "Rodriguez")
+print(my_person.name)
