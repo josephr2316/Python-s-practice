bc683a3a13fd491629ddf41cfc7f3c986056cffc
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2023-05-15 23:54:31 -0400

    Practising with modules and implementing alias

--- 13_Modules/main.py
+++ 13_Modules/main.py
@@ -1,4 +1,23 @@
 ### Modules ###
 
+import my_module
+import math
+import random
+from math import pi as PI_VALUE
+# Python does not accept nomenclature with numbers.
+from my_module import sumValue, printValue
+my_module.sumValue(5, 3, 1)
+my_module.printValue("Hello python!")
 
-my_function()
+sumValue(5, 3, 1)
+sumValue(5, 6, 1)
+sumValue(5, 6, 12)
+
+printValue("Hello Java!")
+
+print(math.pi)
+print(int(math.pow(2,8)))
+# print(pi)
+print(PI_VALUE)
+
+
